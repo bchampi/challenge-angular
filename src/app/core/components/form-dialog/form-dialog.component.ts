cc165ae08b6b5790d30fab58517eaabf90cc14b7
bchampi <brayanchampi27@gmail.com>
2023-01-01 13:21:06 -0500

    Configuración de reglas y package-lock.json

--- src/app/core/components/form-dialog/form-dialog.component.ts
+++ src/app/core/components/form-dialog/form-dialog.component.ts
@@ -1,24 +1,22 @@
-import { Component, Inject, OnInit } from '@angular/core'
+import { Component, Inject } from '@angular/core'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 
 @Component({
   selector: 'app-form-dialog',
   templateUrl: './form-dialog.component.html',
-  styleUrls: ['./form-dialog.component.scss']
+  styleUrls: ['./form-dialog.component.scss'],
 })
 
-export class FormDialogComponent implements OnInit {
-
+export class FormDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<FormDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: string
+    @Inject(MAT_DIALOG_DATA) public data: string,
   ) {}
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
 
   onNoClick(): void {
     this.dialogRef.close()
   }
-
 }
